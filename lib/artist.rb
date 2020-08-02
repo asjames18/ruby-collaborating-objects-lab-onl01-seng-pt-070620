37b509d8a38bb69ee7c8944c89d0e1145d6b1756
Antonio James <asjames18@gmail.com>
2020-08-02 21:23:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,46 +5,43 @@ class Artist
 
 attr_accessor :name, :songs
 
-def initialize (name)
-  @name = name
-  @@all << self
-  @songs = []
-end
+    def initialize (name)
+      @name = name
+      @@all << self
+      @songs = []
+    end
 
-def self.all
-  @@all
-end
-def add_song(song)
-  @songs << song
-end
+    def self.all
+      @@all
+    end
+    def add_song(song)
+      @songs << song
+    end
 
-def songs
-  @songs
+    def songs
+      @songs
+    end
 
-# binding.pry
-end
+    def self.find_or_create_by_name(name)
+      self.find(name) || self.create(name)
+    end
 
-def self.find_or_create_by_name(name)
-  self.find(name) || self.create(name)
-end
+    def self.find(name)
+      self.all.detect { |artist| 
+        artist.name == name }
+    end
 
-def self.find(name)
-  self.all.detect { |artist| artist.name == name }
-end
+    def self.create(name)
+      artist = self.new(name)
+      artist.name = name
+      artist
+      # binding.pry
+    end
 
-def self.create(name)
-  artist = self.new(name)
-  artist.name = name
-  artist
-  # binding.pry
-
-end
-
-def print_songs
-  songs.each { |song|
-    puts song.name}
-
-end
+    def print_songs
+      songs.each { |song|
+        puts song.name}
+    end
 
 
 end
