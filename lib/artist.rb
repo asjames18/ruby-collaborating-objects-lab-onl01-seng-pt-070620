90fffa3a20fec677af001d06ea80b7deb7fae777
Antonio James <asjames18@gmail.com>
2020-08-02 20:07:42 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,74 +1,29 @@
 require 'pry'
 
-# class Artist
-#
-#   @@all = []
-#
-# attr_accessor :name, :songs
-#
-# def initialize (name)
-#   @name = name
-#   @@all << self
-# end
-#
-# def self.all
-#   @@all
-# end
-# def add_song(song)
-#   @songs << song
-# end
-#
-# def songs
-#   @songs
-#
-# # binding.pry
-# end
-#
-#
-# end
-
 class Artist
-  attr_accessor :name, :songs
 
   @@all = []
 
-  def initialize(name)
-    @name = name
-    @songs = []
-    save
-  end
+attr_accessor :name, :songs
 
-  def self.all
-    @@all
-  end
+def initialize (name)
+  @name = name
+  @@all << self
+  songs = []
+end
 
-  # def self.create(name)
-  #   artist = self.new(name)
-  #   artist.name = name
-  #   artist
-  # end
-  #
-  # def self.find(name)
-  #   self.all.detect { |artist| artist.name == name }
-  # end
-  #
-  # def self.find_or_create_by_name(name)
-  #   self.find(name) || self.create(name)
-  # end
+def self.all
+  @@all
+end
+def add_song(song)
+  @songs << song
+end
 
-  def add_song(song)
-    @songs << song
-  end
-  #
-  # def print_songs
-  #   songs.each { |song| puts song.name }
-  # end
+def songs
+  @songs
 
-  def save
-    @@all << self
-  end
+# binding.pry
+end
 
-  def songs
-    @songs
-  end
+
 end
